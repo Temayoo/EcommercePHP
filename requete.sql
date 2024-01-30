2e7d8a799c9cad9ed8ce3556868d7a0e882c819e
Temayoo <Matt.yo95280@gmail.com>
2024-01-30 14:37:33 +0100

    requeteSQL

---
+++ requete.sql
@@ -0,0 +1,46 @@
+-- Création de la table User
+CREATE TABLE User (
+    id INT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL,
+    mdp VARCHAR(255) NOT NULL,
+    pseudo VARCHAR(255) NOT NULL,
+    admin BOOLEAN DEFAULT FALSE,
+    creation TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    action TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+);
+
+
+
+-- Création de la table Produit
+CREATE TABLE Produit (
+    id INT PRIMARY KEY,
+    stock INT,
+    prix FLOAT,
+    genre VARCHAR(255),
+    nom VARCHAR(255),
+    commentaire TEXT,
+    notations FLOAT
+);
+
+
+-- Création de la table Commande
+CREATE TABLE Commande (
+    id INT PRIMARY KEY,
+    id_user INT,
+    id_produit INT,
+    date_commande DATE,
+    description TEXT,
+    FOREIGN KEY (id_user) REFERENCES User(id),
+    FOREIGN KEY (id_produit) REFERENCES Produit(id)
+);
+
+
+-- Création de la table de liaison ProduitCommande
+CREATE TABLE ProduitCommande (
+    id_produit INT,
+    id_commande INT,
+    quantite INT,
+    FOREIGN KEY (id_produit) REFERENCES Produit(id),
+    FOREIGN KEY (id_commande) REFERENCES Commande(id)
+);
+
