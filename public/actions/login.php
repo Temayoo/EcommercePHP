306888cda67c3c2c03235cf38b3311d6b81d6a64
Temayoo <Matt.yo95280@gmail.com>
2024-01-30 16:40:50 +0100

    login good

--- public/actions/login.php
+++ public/actions/login.php
@@ -1,18 +1,35 @@
-<?php 
+<?php
+require_once __DIR__ . '/../../src/init.php';
+// $pdo est dispo !
 
-session_start();
-session_unset();
-session_destroy();
-session_abort();
-$ok = 0;
-if (isset($_POST['rememberme']) && $_POST['rememberme'] === 'yes') {
-    $ok = session_set_cookie_params(1000000);
+if (empty($_POST['email']) || empty($_POST['password'])) {
+    // error
+    $_SESSION['error_message'] = 'Champs email ou mot de passe vide.';
+    header('Location: /login.php'); // redirige utilisateur
+    die(); // stop execution du script
 }
 
-// require ici
+if (filter_var($_POST['email'], FILTER_VALIDATE_EMAIL) === false) {
+    $_SESSION['error_message'] = "L'email est invalide";
+    header('Location: /login.php'); // redirige utilisateur
+    die(); // stop execution du script
+}
 
-session_start();
+$email = $_POST['email'];
+$password = hash('sha256', $_POST['password']);
 
-var_dump($_POST, $ok);
+// Vérifier les informations de connexion
+$st = $pdo->prepare('SELECT * FROM User WHERE email = ? AND mdp = ?');
+$st->execute([$email, $password]);
+$user = $st->fetch(PDO::FETCH_ASSOC);
 
-// traitement classique
+if (!$user) {
+    // Les informations de connexion sont incorrectes
+    $_SESSION['error_message'] = 'Email ou mot de passe incorrect.';
+    header('Location: /login.php');
+    die();
+}
+
+// Les informations de connexion sont correctes, créer une session et rediriger
+$_SESSION['user_id'] = $user['id'];
+header('Location: /dashboard.php'); // Rediriger vers la page du tableau de bord ou toute autre page après la connexion
